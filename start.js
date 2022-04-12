bd147df77347693ffc9830580a8732b7051c5aa6
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2022-04-12 23:45:58 +0300

    fix for Linux/MacOS

--- start.js
+++ start.js
@@ -203,14 +203,14 @@ async function kill() {
                         icon: __dirname + "\\resources\\assets\\icon.ico"
                     },
                     linux: {
-                        filePath: __dirname + "\\Start Light.sh",
+                        filePath: __dirname + "\\StartLight.sh",
                         name: "Light",
                         description: "Light Game Engine",
                         icon: __dirname + "\\resources\\assets\\icon.png",
                         type: "Application"
                     },
                     osx: {
-                        filePath: __dirname + "\\Start Light.sh",
+                        filePath: __dirname + "\\StartLight.sh",
                         name: "Light"
                     }
                 });
